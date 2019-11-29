29e8d79045217a518d1a59518d420296f7c42dcb
RC Rochelle Cohn (####) <rcohn@ag.com>
2019-11-29 16:36:28 -0500

    working on character rotation

--- AoC_WorkingTitle/Assets/Scripts/PlayerMove.cs
+++ AoC_WorkingTitle/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField]
+    private float speedRot;//the rotation of the chracter 
+
+    private Rigidbody rigidBody;//the character 
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = this.GetComponent<Rigidbody>();//get the rb and save it
+        mainCamera = FindObjectOfType<Camera>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void update(){
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * speedRot;
+
+        Ray CameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up,Vector3.zero);
+        float rayLength;
+
+        if(ground.Raycast(CameraRay,out rayLength)){
+            Vector3 pointLook = CameraRay.GetPoint(rayLength);
+            transform.LookAt(pointLook);
+            Debug.DrawLine(CameraRay.origin,pointLook,Color.red);
+        }
+
+    }
+  
+	void FixedUpdate () 
+	{
+    	rigidBody.velocity= moveVelocity;
+
     }
 }
